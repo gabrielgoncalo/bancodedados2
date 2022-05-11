8b2fa2d22457ccff4d7c4f78a17bcb9204c2fe31
gabriel gonçalo <gabriel.tchutcha@hotmail.com.br>
2022-05-11 00:55:21 -0300

    atv 1 e 3 #1

--- tarefa01-inserts.sql
+++ tarefa01-inserts.sql
@@ -1,11 +1,8 @@
-insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('gabriel', 'H', '1997-09-23', 5000.00, 1);
-insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('joao', 'H', '2001-08-31', 1500.00, 2);
-insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('cintia', 'M', '1971-09-07', 3000.00, 3);
-insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('maria', 'M', '1969-05-10', 3000.00, 4);
-insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('alex', 'H', '1955-02-24', 1500.00, 5);
-
-
-
+insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('gabriel', 'H', '04-01-1999', 5000.00, 1);
+insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('joao', 'H', '04-02-1999', 1500.00, 2);
+insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('cintia', 'M', '04-03-2005', 3000.00, 3);
+insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('maria', 'M', '04-04-1997', 3000.00, 4);
+insert into funcionario(nome, sexo, dt_nasc, salario, cod_depto) values('alex', 'H', '04-05-2002', 1500.00, 5);
 
 insert into departamento(descricao, cod_gerente) values('administração', 1);
 insert into departamento(descricao, cod_gerente) values('marketing', 2);
@@ -13,20 +10,20 @@ insert into departamento(descricao, cod_gerente) values('analise', 3);
 insert into departamento(descricao, cod_gerente) values('desenvolvimento', 4);
 insert into departamento(descricao, cod_gerente) values('teste', 5);
 
-insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio) values('gerenciar', 'gerenciar equipe', 1, 1, '2022-05-08');
-insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio) values('', '', 1, 1, '2022-05-08');
-insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio) values('', '', 1, 1, '2022-05-08');
-insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio) values('', '', 1, 1, '2022-05-08');
-insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio) values('', '', 1, 1, '2022-05-08');
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio) values('AD', 'gerenciar equipe', 1, 1, '04-01-2020');
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio) values('MA', 'expandir o marketing', 2, 2, '04-01-2020');
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio) values('AN', 'analisar o sistema', 3, 3, '04-01-2020');
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio) values('DE', 'desenvolver o sistema', 4, 4, '04-01-2020');
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio) values('TE', 'testar o sistema', 5, 5, '04-01-2020');
 
-insert into atividade(nome, descricao, cod_responsavel, data_inicio) values('', '', 1, '2022-05-08');
-insert into atividade(nome, descricao, cod_responsavel, data_inicio) values('Análise do uso dos canais de comunicação pela população', 'Pesquisar indicadores e métricas estatísticas a respeito do uso de cada canal de comunicação', 2, '2022-05-08');
-insert into atividade(nome, descricao, cod_responsavel, data_inicio) values('Contratação de professores capacitados para ofertar os cursos necessários', 'Buscar professores que dominam os temas mais atuais de cada área do conhecimento e contrata-los', 3, '2022-05-08');
-insert into atividade(nome, descricao, cod_responsavel, data_inicio) values('Projeto e Implementação da Topologia de Rede', 'Realizar o design da topologia de rede que será aplicada à nova infraestrutura', 4, '2022-05-08');
-insert into atividade(nome, descricao, cod_responsavel, data_inicio) values('Elaborar um plano de implementação incremental para as novas fontes de energia', 'Instalar e configurar gradativamente os paineis fotovoltaicos', 5, '2022-05-08');
+insert into atividade(nome, descricao, cod_responsavel, data_inicio) values('atv1', 'fazer atividade 1', 4, '04-01-2020');
+insert into atividade(nome, descricao, cod_responsavel, data_inicio) values('atv2', 'fazer atividade 2', 4, '04-01-2020');
+insert into atividade(nome, descricao, cod_responsavel, data_inicio) values('atv3', 'fazer atividade 3', 4, '04-01-2020');
+insert into atividade(nome, descricao, cod_responsavel, data_inicio) values('atv4', 'fazer atividade 4', 4, '04-01-2020');
+insert into atividade(nome, descricao, cod_responsavel, data_inicio) values('atv5', 'fazer atividade 5', 4, '04-01-2020');
 
-insert into atividade_projeto(cod_projeto, cod_atividade) values(1, 1);
-insert into atividade_projeto(cod_projeto, cod_atividade) values(2, 2);
-insert into atividade_projeto(cod_projeto, cod_atividade) values(3, 3);
+insert into atividade_projeto(cod_projeto, cod_atividade) values(4, 1);
+insert into atividade_projeto(cod_projeto, cod_atividade) values(4, 2);
+insert into atividade_projeto(cod_projeto, cod_atividade) values(4, 3);
 insert into atividade_projeto(cod_projeto, cod_atividade) values(4, 4);
-insert into atividade_projeto(cod_projeto, cod_atividade) values(5, 5);
+insert into atividade_projeto(cod_projeto, cod_atividade) values(4, 5);
